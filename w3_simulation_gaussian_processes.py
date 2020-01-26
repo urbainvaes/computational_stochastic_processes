c080de9cb840e2eed0e3f2188b34f9936bb0ffb6
Urbain Vaes <urbain@vaes.uk>
2020-01-26 11:58:30 +0000

    Add initial version of code for w3

---
+++ w3_simulation_gaussian_processes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+#+
+# Copyright (c) 2020 Urbain Vaes. All rights reserved.
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
