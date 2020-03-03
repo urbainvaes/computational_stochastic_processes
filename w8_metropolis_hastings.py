67907c13d373c6af4c06ace2d03930379f86bc05
Urbain Vaes <urbain@vaes.uk>
2020-03-03 00:47:41 +0000

    Add draft

--- w8_metropolis_hastings.py
+++ w8_metropolis_hastings.py
@@ -4,9 +4,7 @@
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
 import numpy as np
-import scipy.stats
-import scipy.integrate
-import scipy.optimize
+import networkx as nx
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -23,3 +21,45 @@ matplotlib.rc('figure.subplot', wspace=.1)
 matplotlib.rc('animation', html='html5')
 np.random.seed(0)
 
+G = nx.DiGraph()
+G.add_edges_from([('A', 'C')], weight=1)
+G.add_edges_from([('B', 'A')], weight=.5)
+G.add_edges_from([('B', 'C')], weight=.6)
+G.add_edges_from([('C', 'B')], weight=.7)
+G.add_edges_from([('C', 'D')], weight=.8)
+G.add_edges_from([('D', 'C')], weight=.9)
+G.add_edges_from([('D', 'E')], weight=.5)
+G.add_edges_from([('E', 'C')], weight=1)
+
+pos = {'A': (0, 0), 'B': (0, 2), 'C': (1, 1), 'D': (2, 0), 'E': (2, 2)}
+
+fig, ax = plt.subplots()
+
+kwargs = {
+        'fontsize': 18,
+        'horizontalalignment': 'center',
+        'verticalalignment': 'center',
+        'transform': ax.transAxes,
+        }
+text = ax.text(.3, .79, "1/2".format(0), **kwargs)
+text = ax.text(.3, .62, "1/2".format(0), **kwargs)
+text = ax.text(.3, .28, "1".format(0), **kwargs)
+text = ax.text(.7, .79, "1".format(0), **kwargs)
+text = ax.text(.7, .38, "1/2".format(0), **kwargs)
+text = ax.text(.7, .20, "1/2".format(0), **kwargs)
+
+labels={}
+labels['A']=r'$a$'
+labels['B']=r'$b$'
+labels['C']=r'$c$'
+labels['D']=r'$d$'
+labels['E']=r'$\alpha$'
+
+labels = {'A': 1000, 'B': 0, 'C': 0, 'D': 0, 'E': 0}
+values = [labels[node] for node in G.nodes()]
+
+nx.draw_networkx_labels(G, pos, labels, font_size=16)
+
+nx.draw(G, pos, node_color=values, node_size=1500, connectionstyle='arc3, rad=0.1', ax=ax)
+# nx.draw(G, pos, node_size=3000, connectionstyle='arc3, rad=0.1', ax=ax)
+plt.show()
